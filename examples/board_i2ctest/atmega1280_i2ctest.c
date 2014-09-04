2190f69580c71bc715a9dd24c27ebcd81d53b29b
Doug Szumski <d.s.szumski@gmail.com>
2014-09-04 22:54:25 +0200

    examples: Extends i2ctest to include a second TWI driver
    Adds a commonly used alternative to the Atmel TWI driver. This
    particular driver is choosen because it covers a non-interrupt
    driven approach to using the TWI module.

--- examples/board_i2ctest/atmega1280_i2ctest.c
+++ examples/board_i2ctest/atmega1280_i2ctest.c
@@ -28,8 +28,11 @@
 AVR_MCU(F_CPU, "atmega1280");
 
 #include "../shared/avr_twi_master.h"
+#include "../shared/twimaster.h"
 
 #include <stdio.h>
+
+#define EEPROM_ADDR	0xA0
 
 static int uart_putchar(char c, FILE *stream) {
   if (c == '\n')
@@ -42,17 +45,14 @@ static int uart_putchar(char c, FILE *stream) {
 static FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL,
                                          _FDEV_SETUP_WRITE);
 
-int main()
+static void
+test_twi_with_atmel_driver(void)
 {
-	stdout = &mystdout;
-
-	sei();
-
 	TWI_Master_Initialise();
 
 	{	// write 2 bytes at some random address
 		uint8_t msg[8] = {
-				0xa0, // TWI address,
+				EEPROM_ADDR, // TWI address,
 				0xaa, 0x01, // eeprom address, in little endian
 				0xde, 0xad,	// data bytes
 		};
@@ -63,7 +63,7 @@ int main()
 	}
 	{
 		uint8_t msg[8] = {
-				0xa0, // TWI address,
+				EEPROM_ADDR, // TWI address,
 				0xa8, 0x01, // eeprom address, in little endian
 		};
 		TWI_Start_Transceiver_With_Data(msg, 3, 0); // dont send stop!
@@ -73,14 +73,68 @@ int main()
 	}
 	{
 		uint8_t msg[9] = {
-				0xa0 + 1, // TWI address,
+				EEPROM_ADDR + 1, // TWI address,
 		};
 		TWI_Start_Transceiver_With_Data(msg, 9, 1); // write 1 byte, read 8, send stop
 
 		while (TWI_Transceiver_Busy())
 			sleep_mode();
 	}
+}
+
+/*
+ * Tests the TWI using a commonly used and non-interrupt driven
+ * alternative to the Atmel driver:
+ *
+ * "I2C master library using hardware TWI interface"
+ * Author:   Peter Fleury <pfleury@gmx.ch>  http://jump.to/fleury
+ */
+static void
+test_twi_with_pf_driver(void)
+{
+	/*
+	 * This init followed by a start condition is enough to overwrite all TWI
+	 * related bits set by TWI_Master_Initialise () in the Atmel driver.
+	 */
+	i2c_init();
+
+	i2c_start(EEPROM_ADDR + I2C_WRITE);
+	// eeprom address, in little endian
+	i2c_write(0xaa);
+	i2c_write(0x01);
+	// data bytes
+	i2c_write(0xd0);
+	i2c_write(0x0d);
+	i2c_stop();
+
+	i2c_start(EEPROM_ADDR + I2C_WRITE);
+	// set address
+	i2c_write(0xa8);
+	i2c_write(0x01);
+	// Don't stop
+
+	// Read back data
+	i2c_start (EEPROM_ADDR + I2C_READ);
+	for (uint8_t i = 0; i < 8; ++i) {
+		i2c_readNak();
+	};
+	i2c_stop();
+}
+
+int main()
+{
+	stdout = &mystdout;
+
+	sei();
+
+	test_twi_with_atmel_driver();
+
+	/*
+	 * This should produce *exactly* the same output as above, except
+	 * the data written should be D00D as opposed to DEAD.
+	 */
+	test_twi_with_pf_driver ();
+
 	cli();
 	sleep_mode();
 }
-
