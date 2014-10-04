8d08bbed9ab003b94e1bcb6b061628c24313fc7e
bsekisser <squirmyworms@embarqmail.com>
2014-10-04 09:08:28 -0400

    fix initialization of port_a on atmega16 devices.
    	modified:   cores/sim_megax.c
    		replace use of device specific macro with check
    			for (in theory) ready to initialize port_a
    			structure.

--- simavr/cores/sim_megax.c
+++ simavr/cores/sim_megax.c
@@ -32,9 +32,8 @@ void mx_init(struct avr_t * avr)
 	avr_flash_init(avr, &mcu->selfprog);
 	avr_watchdog_init(avr, &mcu->watchdog);
 	avr_extint_init(avr, &mcu->extint);
-#ifdef PORTA
-	avr_ioport_init(avr, &mcu->porta);
-#endif
+	if((mcu->porta.name == 'A') && (mcu->porta.r_port != 0))
+		avr_ioport_init(avr, &mcu->porta);
 	avr_ioport_init(avr, &mcu->portb);
 	avr_ioport_init(avr, &mcu->portc);
 	avr_ioport_init(avr, &mcu->portd);
