a37c1bed9ccc8125cd1732b214a6858391372ad0
Michel Pollet <buserror@gmail.com>
2017-02-17 09:56:41 +0000

    Updated some tests to use the new VCD macros
    No functional changes
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

--- tests/atmega2560_uart_echo.c
+++ tests/atmega2560_uart_echo.c
@@ -37,6 +37,8 @@ const struct avr_mmcu_vcd_trace_t _mytrace[]  _MMCU_ = {
 	{ AVR_MCU_VCD_SYMBOL("UDRE3"), .mask = (1 << UDRE3), .what = (void*)&UCSR3A, },
 	{ AVR_MCU_VCD_SYMBOL("GPIOR1"), .what = (void*)&GPIOR1, },
 };
+AVR_MCU_VCD_IRQ(USART3_RX);	// single bit trace
+AVR_MCU_VCD_ALL_IRQ();		// also show ALL irqs running
 
 volatile uint8_t cnt = 0;
 volatile uint8_t done = 0;
