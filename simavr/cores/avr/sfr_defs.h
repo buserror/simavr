d974495a734a5bb5b864974e7aadca8cf32f85fc
Michel Pollet <buserror@gmail.com>
2017-08-02 11:04:55 +0100

    Add a stub sfr_defs.h
    Some avrlibc cores include that file, that define macros and so on. We
    don't need/want that as we already have macros for stuff
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

---
+++ simavr/cores/avr/sfr_defs.h
@@ -0,0 +1,27 @@
+/*
+	STUB FILE FOR sfr_defs.h
+
+	Copyright 2008, 2009 Michel Pollet <buserror@gmail.com>
+
+ 	This file is part of simavr.
+
+	simavr is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	simavr is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with simavr.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* STUB file, the orignal is included in two cores, and mess up compilation */
+
+#ifndef _AVR_SFR_DEFS_H_
+#define _AVR_SFR_DEFS_H_ 1
+
+#endif  /* _SFR_DEFS_H_ */
