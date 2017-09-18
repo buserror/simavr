262574de752ccb9f41efe73a6d0ded69bb02154a
Torbjorn Tyridal <ttyridal@cisco.com>
2017-09-18 09:38:54 +0200

    Add license headers to all files

--- examples/parts/usbip_tester.py
+++ examples/parts/usbip_tester.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 #
+#	Copyright 2017 Torbjorn Tyridal <ttyridal@gmail.com>
+#
+#	This file is part of simavr.
+#
+#	simavr is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	simavr is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with simavr.  If not, see <http://www.gnu.org/licenses/>.
+#
 # This is a quickly thrown together emulation of the kernel + usbip utilities
 # side of the usbip protocol
 #
--- examples/parts/usbip_types.h
+++ examples/parts/usbip_types.h
@@ -1,3 +1,25 @@
+/* vim: set sts=4:sw=4:ts=4:noexpandtab
+	usbip_types.h
+
+	Copyright 2017 Torbjorn Tyridal <ttyridal@gmail.com>
+
+	This file is part of simavr.
+
+	simavr is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	simavr is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with simavr.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 //ref https://github.com/torvalds/linux/blob/master/tools/usb/usbip
 //ref https://github.com/torvalds/linux/blob/master/drivers/usb/usbip
 
